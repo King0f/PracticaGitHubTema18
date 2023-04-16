5955785de577c52ba5d9d778823cb0cabf14d274
King0f <nicogm03@gmail.com>
2023-04-16 22:32:03 +0200

    0.1 D 2

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("v0.1");
-        System.out.println("develop");
+        System.out.println("develop 2");
     }
 }
