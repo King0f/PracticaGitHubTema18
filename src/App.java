6b9922842d1eeb462b01389dc66e1ac0a6eec7a8
King0f <nicogm03@gmail.com>
2023-04-16 22:31:32 +0200

    F 4

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("v0.1");
-        System.out.println("DEVELOP FEATURE 3");
+        System.out.println("DEVELOP FEATURE 4");
     }
 }
